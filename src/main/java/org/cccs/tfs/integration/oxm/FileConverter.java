49439a95c455c629343ff2bd07e1aa69d7d33878
Craig Cook <boycook@me.com>
2011-03-30 12:36:06 +0100

    Adding in file XLM converters

---
+++ src/main/java/org/cccs/tfs/integration/oxm/FileConverter.java
@@ -0,0 +1,110 @@
+package org.cccs.tfs.integration.oxm;
+
+import com.thoughtworks.xstream.converters.Converter;
+import com.thoughtworks.xstream.converters.MarshallingContext;
+import com.thoughtworks.xstream.converters.UnmarshallingContext;
+import com.thoughtworks.xstream.io.HierarchicalStreamReader;
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
+import org.cccs.tfs.domain.File;
+
+import static org.cccs.tfs.utils.RuntimeSupport.isNotEmpty;
+
+/**
+ * User: boycook
+ * Date: 30/03/2011
+ * Time: 12:19
+ */
+public class FileConverter implements Converter {
+    @Override
+    public void marshal(Object o, HierarchicalStreamWriter writer, MarshallingContext context) {
+        File f = (File) o;
+
+        if (f.getId() != 0) {
+            writer.addAttribute("id", String.valueOf(f.getId()));
+            writer.startNode("id");
+            writer.setValue(String.valueOf(f.getId()));
+            writer.endNode();
+        }
+
+        writer.startNode("groupId");
+        writer.setValue(f.getGroupId());
+        writer.endNode();
+
+        writer.startNode("artefactId");
+        writer.setValue(f.getArtefactId());
+        writer.endNode();
+
+        writer.startNode("version");
+        writer.setValue(f.getVersion());
+        writer.endNode();
+
+        writer.startNode("url");
+        writer.setValue(f.getUrl());
+        writer.endNode();
+
+        if (isNotEmpty(f.getName())) {
+            writer.startNode("name");
+            writer.setValue(f.getName());
+            writer.endNode();
+        }
+
+        if (isNotEmpty(f.getDescription())) {
+            writer.startNode("description");
+            writer.setValue(f.getDescription());
+            writer.endNode();
+        }
+
+        if (isNotEmpty(f.getStorageType())) {
+            writer.startNode("storageType");
+            writer.setValue(f.getStorageType());
+            writer.endNode();
+        }
+
+        if (isNotEmpty(f.getWebsite())) {
+            writer.startNode("website");
+            writer.setValue(f.getWebsite());
+            writer.endNode();
+        }
+    }
+
+    @Override
+    public Object unmarshal(HierarchicalStreamReader reader, UnmarshallingContext context) {
+        final File f = new File();
+
+        while (reader.hasMoreChildren()) {
+            reader.moveDown();
+
+            String node = reader.getNodeName();
+            String value = reader.getValue();
+
+            if (node.equalsIgnoreCase("id")) {
+                f.setId(Long.valueOf(value));
+            } else if (node.equalsIgnoreCase("groupId")) {
+                f.setGroupId(value);
+            } else if (node.equalsIgnoreCase("artefactId")) {
+                f.setArtefactId(value);
+            } else if (node.equalsIgnoreCase("version")) {
+                f.setVersion(value);
+            } else if (node.equalsIgnoreCase("url")) {
+                f.setUrl(value);
+            } else if (node.equalsIgnoreCase("name")) {
+                f.setName(value);
+            } else if (node.equalsIgnoreCase("description")) {
+                f.setDescription(value);
+            } else if (node.equalsIgnoreCase("storageType")) {
+                f.setStorageType(value);
+            } else if (node.equalsIgnoreCase("website")) {
+                f.setWebsite(value);
+            }
+
+            reader.moveUp();
+        }
+
+        return f;
+    }
+
+    @Override
+    public boolean canConvert(Class aClass) {
+        return aClass == File.class;
+    }
+}
---
+++ src/test/groovy/org/cccs/tfs/integration/oxm/TestFileConverter.groovy
@@ -0,0 +1,97 @@
+package org.cccs.tfs.integration.oxm
+
+import org.springframework.test.context.ContextConfiguration
+import org.junit.runner.RunWith
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+import org.custommonkey.xmlunit.XMLTestCase
+import org.springframework.oxm.xstream.XStreamMarshaller
+import org.springframework.beans.factory.annotation.Autowired
+import org.cccs.tfs.finder.FileFinder
+import org.custommonkey.xmlunit.XMLUnit
+import org.junit.Test
+import javax.xml.transform.stream.StreamSource
+import javax.xml.transform.stream.StreamResult
+import javax.xml.transform.Result
+
+/**
+ * User: boycook
+ * Date: 30/03/2011
+ * Time: 12:24
+ */
+@ContextConfiguration(locations = "classpath:testApplicationContext.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+class TestFileConverter extends XMLTestCase {
+
+    static {
+        XMLUnit.setIgnoreWhitespace(true);
+        XMLUnit.setIgnoreAttributeOrder(true);
+    }
+
+    @Autowired
+    private FileFinder finder;
+
+    @Autowired
+    private XStreamMarshaller marshaller;
+
+    private String expectedList = """
+        <resources>
+            <file id="12">
+                <id>12</id>
+                <groupId>org.cccs.jslibs</groupId>
+                <artefactId>jquery.collapsible</artefactId>
+                <version>1.0</version>
+                <url>https://github.com/BoyCook/JSLibs/raw/master/jquery.collapsible/lib/jquery.collapsible.js</url>
+            </file>
+            <file id="13">
+                <id>13</id>
+                <groupId>org.cccs.jslibs</groupId>
+                <artefactId>jquery.collapsible</artefactId>
+                <version>1.0.0</version>
+                <url>https://github.com/BoyCook/JSLibs/raw/master/jquery.madeup/lib/jquery.collapsible.js</url>
+            </file>
+        </resources>
+    """
+
+    private String expectedFile = """
+        <file id="12">
+            <id>12</id>
+            <groupId>org.cccs.jslibs</groupId>
+            <artefactId>jquery.collapsible</artefactId>
+            <version>1.0</version>
+            <url>https://github.com/BoyCook/JSLibs/raw/master/jquery.collapsible/lib/jquery.collapsible.js</url>
+        </file>
+    """
+
+    @Test
+    public void marshallingListShouldWork() throws IOException {
+        String marshalled = marshal(finder.search("jquery.collapsible"));
+        println marshalled;
+        assertXMLEqual("Comparing marshalled xml to expected xml", expectedList, marshalled);
+    }
+
+    @Test
+    public void marshallingAFileShouldWork() throws IOException {
+        String marshalled = marshal(finder.search("jquery.collapsible").get(0));
+        println marshalled;
+        assertXMLEqual("Comparing marshalled xml to expected xml", expectedFile, marshalled);
+    }
+
+    private String marshal(final Object obj) throws IOException {
+        final ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        marshal(obj, new BufferedOutputStream(bos));
+        return bos.toString();
+    }
+
+    private void marshal(final Object obj, final OutputStream stream) throws IOException {
+        final Result result = new StreamResult(stream);
+        marshaller.marshal(obj, result);
+    }
+
+    public Object unmarshal(final String data) throws IOException {
+        return unmarshal(new ByteArrayInputStream(data.getBytes()));
+    }
+
+    public Object unmarshal(final InputStream stream) throws IOException {
+        return marshaller.unmarshal(new StreamSource(stream));
+    }
+}
