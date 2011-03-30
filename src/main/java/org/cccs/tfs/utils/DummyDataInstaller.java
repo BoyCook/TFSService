61cfe73be1866b041af0d5feb66f367ffd669883
Craig Cook <boycook@me.com>
2011-03-30 01:12:45 +0100

    Added file finder test

--- src/main/java/org/cccs/tfs/utils/DummyDataInstaller.java
+++ src/main/java/org/cccs/tfs/utils/DummyDataInstaller.java
@@ -1,6 +1,8 @@
 package org.cccs.tfs.utils;
 
+import org.cccs.tfs.domain.File;
 import org.cccs.tfs.domain.Principal;
+import org.cccs.tfs.service.FileService;
 import org.cccs.tfs.service.PrincipalService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,19 +18,27 @@ public class DummyDataInstaller implements Installer {
     protected final Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
-    public PrincipalService service;
+    public PrincipalService principalService;
+
+    @Autowired
+    public FileService fileService;
 
     public DummyDataInstaller() {}
 
-    public DummyDataInstaller(PrincipalService service) {
-        this.service = service;
+    public DummyDataInstaller(PrincipalService principalService) {
+        this.principalService = principalService;
     }
 
     public void install() {
         try {
-            service.create(new Principal("BoyCook", "Boy", "Cook", "password", "boycook@me.com", "07918880501"));
-            service.create(new Principal("CraigCook", "Craig", "Cook", "password", "craig.cook@bt.com", "07918880501"));
-            service.create(new Principal("FooBar", "Foo", "Bar", "password", "craig.cook@bt.com", "07918880501"));
+            principalService.create(new Principal("BoyCook", "Boy", "Cook", "password", "boycook@me.com", "07918880501"));
+            principalService.create(new Principal("CraigCook", "Craig", "Cook", "password", "craig.cook@bt.com", "07918880501"));
+            principalService.create(new Principal("FooBar", "Foo", "Bar", "password", "craig.cook@bt.com", "07918880501"));
+
+            fileService.create(new File("org.cccs.jslibs", "jsmap", "1.0", "https://github.com/BoyCook/JSLibs/raw/master/map/lib/jsMap.js"));
+            fileService.create(new File("org.cccs.jslibs", "jquery.hintbox", "1.0", "https://github.com/BoyCook/JSLibs/raw/master/jquery.hintbox/lib/jquery.hintbox.js"));
+            fileService.create(new File("org.cccs.jslibs", "jquery.collapsible", "1.0", "https://github.com/BoyCook/JSLibs/raw/master/jquery.collapsible/lib/jquery.collapsible.js"));
+            fileService.create(new File("org.cccs.jslibs", "jquery.collapsible", "1.0.0", "https://github.com/BoyCook/JSLibs/raw/master/jquery.madeup/lib/jquery.collapsible.js"));
         } catch (Exception e) {
             log.debug("Error installing data");
             log.debug(e.getMessage());
---
+++ src/test/java/org/cccs/tfs/finder/TestFileFinder.java
@@ -0,0 +1,59 @@
+package org.cccs.tfs.finder;
+
+import org.cccs.tfs.domain.File;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * User: boycook
+ * Date: 29/03/2011
+ * Time: 23:41
+ */
+@ContextConfiguration(locations = "classpath:testApplicationContext.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+public class TestFileFinder {
+
+    @Autowired
+    private FileFinder finder;
+
+    @Test
+    public void getAllShouldWork() throws Exception {
+        assertThat(finder.all().size(), is(equalTo(4)));
+    }
+
+    @Test
+    public void searchByArtefactIdShouldWork() throws Exception {
+        List<File> files = finder.search("jsmap");
+        assertThat(files.size(), is(equalTo(1)));
+    }
+
+    @Test
+    public void searchByParamsShouldWork() throws Exception {
+        Map<String, String[]> parameters = new HashMap<String, String[]>();
+        parameters.put("groupId", new String[] {"org.cccs.jslibs"});
+        parameters.put("version", new String[] {"1.0.0"});
+
+        List<File> files = finder.search(parameters);
+        assertThat(files.size(), is(equalTo(1)));
+    }
+
+    @Test
+    public void filterByParamsShouldWork() throws Exception {
+        Map<String, String[]> parameters = new HashMap<String, String[]>();
+        parameters.put("artefactId", new String[] {"jquery.collapsible", "jquery.hintbox"});
+
+        List<File> files = finder.filter(parameters);
+        assertThat(files.size(), is(equalTo(3)));
+    }
+}
